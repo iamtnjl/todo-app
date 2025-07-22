14e29ce689f23dd42db19553676c5d9ad7da2949
iamtnjl <md.tanjil@repliq.dev>
2025-07-23 02:59:08 +0600

    Fix build error

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    css: {
-      lightningcss: false,
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
