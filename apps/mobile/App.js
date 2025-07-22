3a9db7cff2c1d440b1ce1dbf6cc046aba5f8bbea
iamtnjl <md.tanjil@repliq.dev>
2025-07-23 01:12:46 +0600

    Update node v

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import "./global.css"
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView
-      className="flex-1 bg-blue-100 items-center justify-center"
-      edges={['top', 'left', 'right']}
-    >
-      <Text className="text-blue-700 text-lg font-bold">
-        NativeWind is working!
-      </Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
